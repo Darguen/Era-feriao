5df959646f70d6680dc71d234322fc8ed82083fd
Darguen <pvidela17@alumnos.utalca.cl>
2023-12-17 17:45:53 -0300

    Listener Image Button

--- app/src/main/java/cl/brownarmoryelling/era_feriao/MainActivity.kt
+++ app/src/main/java/cl/brownarmoryelling/era_feriao/MainActivity.kt
@@ -5,17 +5,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.widget.AppCompatImageButton
 import cl.brownarmoryelling.era_feriao.Api.FeriadosApi
 import cl.brownarmoryelling.era_feriao.Background.ApiCallback
 import cl.brownarmoryelling.era_feriao.Classes.Feriado
 
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var filterButton: AppCompatImageButton
+    private lateinit var faqButton: AppCompatImageButton
+    private lateinit var aboutButton: AppCompatImageButton
+    private lateinit var holidayButton: AppCompatImageButton
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        filterButton = findViewById(R.id.ImgBut_filter)
+        faqButton = findViewById(R.id.ImgBut_faq)
+        aboutButton = findViewById(R.id.ImgBut_about)
+        holidayButton = findViewById(R.id.ImgBut_holyday)
+
+        filterButton.setOnClickListener{
+            val filterB = Intent(this, ResultadoFeriadoPorFiltro::class.java)
+            startActivity(filterB)
+        }
+        faqButton.setOnClickListener{
+            val faqB = Intent(this, Faq::class.java)
+            startActivity(faqB)
+        }
+        aboutButton.setOnClickListener{
+            val aboutB = Intent(this, About::class.java)
+            startActivity(aboutB)
+        }
+        holidayButton.setOnClickListener{
+            val holidayB = Intent(this, FeriadoMasCercano::class.java)
+            startActivity(holidayB)
+        }
 
         val feriadosApi = FeriadosApi()
         feriadosApi.getData( "2023", "01", "01", "5", "5")
