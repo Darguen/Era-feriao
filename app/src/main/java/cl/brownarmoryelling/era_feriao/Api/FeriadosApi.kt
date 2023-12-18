09d53e167ac0984c9c21e6ca8883d992f8864fc0
Darguen <pvidela17@alumnos.utalca.cl>
2023-12-17 22:34:02 -0300

    FeriadosApi sin los System.err

--- app/src/main/java/cl/brownarmoryelling/era_feriao/Api/FeriadosApi.kt
+++ app/src/main/java/cl/brownarmoryelling/era_feriao/Api/FeriadosApi.kt
@@ -1,10 +1,12 @@
 package cl.brownarmoryelling.era_feriao.Api
 
+import android.util.Log
 import cl.brownarmoryelling.era_feriao.Background.ApiCallback
 import cl.brownarmoryelling.era_feriao.Background.ApiTask
 import cl.brownarmoryelling.era_feriao.Classes.Feriado
 import org.json.JSONArray
 import org.json.JSONException
+import org.json.JSONObject
 
 class FeriadosApi : ApiCallback
 {
@@ -34,40 +36,61 @@ class FeriadosApi : ApiCallback
         return apiRequest(url, this)
     }
 
-    private fun processingData(result : String) : MutableList<Feriado>
+    fun processingData(result : String) : MutableList<Feriado>
     {
         val list : MutableList<Feriado> = mutableListOf()
+        Log.i("FeriadosApi", "JSON Response: $result")
 
-        try
-        {
-            val jsonArray = JSONArray(result)
+        try {
+            if (result.startsWith("{")) {
+                // It's a JSON object, not an array
+                val jsonObject = JSONObject(result)
+                val error = jsonObject.getBoolean("error")
+                val message = jsonObject.getString("message")
 
-            for (i in 0 until jsonArray.length())
-            {
-                val jsonObject = jsonArray.getJSONObject(i)
+                if (error) {
+                    println("Error: $message")
+                    // Handle the error case as needed
+                } else {
+                    // No error, proceed with parsing the array if present
+                    if (jsonObject.has("feriados")) {
+                        val feriadosArray = jsonObject.getJSONArray("feriados")
+                        for (i in 0 until feriadosArray.length()) {
+                            val feriadoObject = feriadosArray.getJSONObject(i)
 
-                val nombre = jsonObject.getString("nombre")
-                val comentarios = jsonObject.getString("comentarios")
-                val fecha = jsonObject.getString("fecha")
-                val irrenunciable = jsonObject.getString("irrenunciable")
-                val tipo = jsonObject.getString("tipo")
+                            val nombre = jsonObject.getString("nombre")
+                            val comentarios = jsonObject.getString("comentarios")
+                            val fecha = jsonObject.getString("fecha")
+                            val irrenunciable = jsonObject.getString("irrenunciable")
+                            val tipo = jsonObject.getString("tipo")
 
-                val leyesArray = jsonObject.getJSONArray("leyes")
-                var leyesNombre : ArrayList<String>? = arrayListOf()
-                var leyesURL : ArrayList<String>? = arrayListOf()
+                            val leyesArray = jsonObject.getJSONArray("leyes")
+                            var leyesNombre: ArrayList<String>? = arrayListOf()
+                            var leyesURL: ArrayList<String>? = arrayListOf()
 
-                for (j in 0 until leyesArray.length())
-                {
-                    val leyObject = leyesArray.getJSONObject(j)
-                    val leyNombre = leyObject.getString("nombre")
-                    val leyUrl = leyObject.getString("url")
+                            for (j in 0 until leyesArray.length()) {
+                                val leyObject = leyesArray.getJSONObject(j)
+                                val leyNombre = leyObject.getString("nombre")
+                                val leyUrl = leyObject.getString("url")
 
-                    leyesNombre?.add(leyNombre)
-                    leyesURL?.add(leyUrl)
+                                leyesNombre?.add(leyNombre)
+                                leyesURL?.add(leyUrl)
+                            }
+
+                            val feriado = Feriado(
+                                nombre,
+                                comentarios,
+                                fecha,
+                                irrenunciable.toBoolean(),
+                                tipo,
+                                leyesNombre,
+                                leyesURL
+                            )
+                            list.add(feriado)
+                        }
+                    }
                 }
 
-                val feriado = Feriado(nombre, comentarios, fecha, irrenunciable.toBoolean(), tipo, leyesNombre, leyesURL)
-                list.add(feriado)
             }
         }
         catch(e : JSONException)
